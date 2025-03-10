63dec57520c24b079354287350f3325df7a82d50
Gabriel Rodriguez Perez <gabrielrperez47@gmail.com>
2025-03-10 19:42:49 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-<script>
-    document.addEventListener("scroll", function () {
-        let navbar = document.querySelector("nav");
-        let firstSection = document.getElementById("section");
+// Hide and show nav bar when scrolling to the about page
+document.addEventListener("scroll", function () {
+        let navbar = document.querySelector("nav-bar");
+        let firstSection = document.getElementById("about");
 
         if (window.scrollY > firstSection.clientHeight - 50) {
             navbar.classList.remove("hidden-nav");  // Show navbar
@@ -9,4 +9,3 @@
             navbar.classList.add("hidden-nav");  // Hide navbar
         }
     });
-</script>
